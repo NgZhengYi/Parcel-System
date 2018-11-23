8a70da61b628dfc1a06bac2036d1ae7cb5c92b79
坏坏丶乔乔 <33797234+NgZhengYi@users.noreply.github.com>
2018-11-23 09:25:04 +0800

    Add files via upload

---
+++ Parcel System/Student/StudentMainMenu.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1">
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <title>Main Menu</title>
+</head>
+
+<body>
+<?php include_once '../Include/StudentNavbar.php' ?>
+
+
+
+
+
+<link rel="stylesheet" type="text/css" href="../CSS/Styles.css">
+</body>
+
+</html>
